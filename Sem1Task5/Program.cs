3c80a93d89d024e6be7edf38c63baaa977518686
Sherman <jagr1477@mail.ru>
2023-02-12 17:05:08 +0300

    add solution for task 0,1,3,5,7

---
+++ Sem1Task5/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача #5
+// Напишите программу, которая на вход принимает одно число (N),
+// а на выходе показывает все целые числа в промежутке от -N до N.
+
+Console.Write("Введение число: ");
+var day = Convert.ToInt32(Console.ReadLine());
+var negDay = -day;
+
+for (; negDay <= day; negDay++)
+{
+    Console.Write(negDay+ "\t");
+}
---
+++ Sem1Task7/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача #7
+// Напишите программу, которая принимает на вход трёхначное число
+// и на выходе показывает последнее цифру этого числа.
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 99 && number <1000)
+{
+    Console.WriteLine("Пследняя цифра числа: "+number%10);
+}
+else
+Console.WriteLine("Введено не то число");
